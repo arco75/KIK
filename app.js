9bf9abe0168bea7dad87cb0f06acfe9e688b7809
Artur Pelo <pelo.artur@gmail.com>
2023-06-03 14:26:11 +0200

    próba implementacji znacznika wstawianego znaku

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-/** TicTacToe v.0.6
+/****************************************************************
+ * TicTacToe v.0.6
  * 
  * Artur Pelo / 03.06.2023
  * artur.pelo@gmail.com
@@ -13,13 +14,14 @@ const button = document.querySelector("#button");
 
 button.addEventListener("click",startGry);
 
-/**
+/****************************************************************
  * Uruchamienie interakcji na obiekty <td>.
  */
 function startGry(){
 
     document.querySelectorAll(".gracz")[0].disabled=true;  
-    document.querySelectorAll(".gracz")[1].disabled=true;  
+    document.querySelectorAll(".gracz")[1].disabled=true; 
+    button.disabled=true;
     for(let n = 0; n<pole.length; n++){
         pole[n].addEventListener("mouseover", zaznacz);
         pole[n].addEventListener("mouseout", zaznaczOFF);
@@ -27,8 +29,7 @@ function startGry(){
     }
 }
 
-
-/**
+/*****************************************************************
  * Funkcja zaznacza obszar po najechaniu myszką
  * @param {pointer} event - obiekt na którym dokonano zdarzenia
  */
@@ -36,25 +37,28 @@ function zaznacz(event){
     event.target.style.border = "1px solid #ff0000";
 }
 
-/**
+/****************************************************************
  * Funckja kasująca zaznaczenie obszar po zjechaniu myszki
  * @param {pointer} event - obiekt na którym dokonano zdarzenia
  */
 function zaznaczOFF(event){
     event.target.style.border = "1px solid #0000aa";
 }
-/**
+/****************************************************************
  * Funkcja wstawiająca znak w obszar kliknięcia
  * @param {pointer} event - wskaźnik na obiekt
  */
 function wstawZnak(event){
-    if(znak) {
-        event.target.innerHTML="X";
-        event.target.style.backgroundColor="#FFAAAA";
-        znak=false;
-    }else{
-        event.target.innerHTML="O";
-        event.target.style.backgroundColor="#AAAAFF";
-        znak=true;
+    console.dir(event.target);
+    if(event.target.innerHTML==""){
+        if(znak) {
+            event.target.innerHTML="X";
+            event.target.style.backgroundColor="#FFAAAA";
+            znak=false;
+        }else{
+            event.target.innerHTML="O";
+            event.target.style.backgroundColor="#AAAAFF";
+            znak=true;
+        };
     };
 }
