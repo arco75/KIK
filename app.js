61aaf69d09735d28b2441b8336da3b18b9b56c3d
Artur Pelo <pelo.artur@gmail.com>
2023-06-03 11:21:40 +0200

    Dodano wstawianie znaków w pola gry

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-//Sekcja podpiecia zdarzeń po elementy td
+//Sekcja podpiecia zdarzeń do elementów td i definicji zmiennych
 
+var znak=true;
 const pole = document.querySelectorAll("td");
 
 for(let n = 0; n<pole.length; n++){
     pole[n].addEventListener("mouseover", zaznacz);
     pole[n].addEventListener("mouseout", zaznaczOFF);
+    pole[n].addEventListener("click",wstawZnak);
 }
 
 /**
@@ -12,7 +14,7 @@ for(let n = 0; n<pole.length; n++){
  * @param {pointer} event - obiekt na którym dokonano zdarzenia
  */
 function zaznacz(event){
-    event.target.style.backgroundColor = "#ffaaaa";
+    event.target.style.border = "1px solid #ff0000";
 }
 
 /**
@@ -20,5 +22,20 @@ function zaznacz(event){
  * @param {pointer} event - obiekt na którym dokonano zdarzenia
  */
 function zaznaczOFF(event){
-    event.target.style.backgroundColor = "";
+    event.target.style.border = "1px solid #0000aa";
+}
+/**
+ * Funkcja wstawiająca znak w obszar kliknięcia
+ * @param {pointer} event - wskaźnik na obiekt
+ */
+function wstawZnak(event){
+    if(znak) {
+        event.target.innerHTML="X";
+        event.target.style.backgroundColor="#FFAAAA";
+        znak=false;
+    }else{
+        event.target.innerHTML="O";
+        event.target.style.backgroundColor="#AAAAFF";
+        znak=true;
+    };
 }
